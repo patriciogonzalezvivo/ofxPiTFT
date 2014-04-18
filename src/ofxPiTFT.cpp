944e61acc74930b3e3dd2a809e859add590a6e9c
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2014-04-18 09:57:05 -0400

    no .c

--- src/ofxPiTFT.cpp
+++ src/ofxPiTFT.cpp
@@ -7,8 +7,10 @@
 //
 
 #include "ofxPiTFT.h"
+
 #define KWHT  "\x1B[37m"
 #define KYEL  "\x1B[33m"
+int fd;
 
 ofxPiTFT::ofxPiTFT(){
     
@@ -90,18 +92,99 @@ ofxPiTFT::~ofxPiTFT(){
     ofRemoveListener(ofEvents().exit,this,&ofxPiTFT::exit);
 }
 
+
+#ifdef TARGET_RASPBERRY_PI
+//void getTouchScreenDetails(int *screenXmin,int *screenXmax,int *screenYmin,int *screenYmax){
+//	unsigned short id[4];
+//    unsigned long bit[EV_MAX][NBITS(KEY_MAX)];
+//    char name[256] = "Unknown";
+//    int abs[6] = {0};
+//    
+//    ioctl(fd, EVIOCGNAME(sizeof(name)), name);
+//    printf("Input device name: \"%s\"\n", name);
+//    
+//    memset(bit, 0, sizeof(bit));
+//    ioctl(fd, EVIOCGBIT(0, EV_MAX), bit[0]);
+//    printf("Supported events:\n");
+//    
+//    int i,j,k;
+//    for (i = 0; i < EV_MAX; i++)
+//        if (test_bit(i, bit[0])) {
+//            printf("  Event type %d (%s)\n", i, events[i] ? events[i] : "?");
+//            if (!i) continue;
+//            ioctl(fd, EVIOCGBIT(i, KEY_MAX), bit[i]);
+//            for (j = 0; j < KEY_MAX; j++){
+//                if (test_bit(j, bit[i])) {
+//                    printf("    Event code %d (%s)\n", j, names[i] ? (names[i][j] ? names[i][j] : "?") : "?");
+//                    if (i == EV_ABS) {
+//                        ioctl(fd, EVIOCGABS(j), abs);
+//                        for (k = 0; k < 5; k++)
+//                            if ((k < 3) || abs[k]){
+//                                printf("     %s %6d\n", absval[k], abs[k]);
+//                                if (j == 0){
+//                                    if (absval[k] == "Min  ") *screenXmin =  abs[k];
+//                                    if (absval[k] == "Max  ") *screenXmax =  abs[k];
+//                                }
+//                                if (j == 1){
+//                                    if (absval[k] == "Min  ") *screenYmin =  abs[k];
+//                                    if (absval[k] == "Max  ") *screenYmax =  abs[k];
+//                                }
+//                            }
+//                    }
+//                    
+//                }
+//            }
+//        }
+//}
+//
+//void getTouchSample(int *rawX, int *rawY, int *rawPressure){
+//	int i;
+//    /* how many bytes were read */
+//    size_t rb;
+//    /* the events (up to 64 at once) */
+//    struct input_event ev[64];
+//    
+//	rb=read(fd,ev,sizeof(struct input_event)*64);
+//    for (i = 0;  i <  (rb / sizeof(struct input_event)); i++){
+//        if (ev[i].type ==  EV_SYN)
+//            printf("Event type is %s%s%s = Start of New Event\n",KYEL,events[ev[i].type],KWHT);
+//        
+//        else if (ev[i].type == EV_KEY && ev[i].code == 330 && ev[i].value == 1)
+//            printf("Event type is %s%s%s & Event code is %sTOUCH(330)%s & Event value is %s1%s = Touch Starting\n", KYEL,events[ev[i].type],KWHT,KYEL,KWHT,KYEL,KWHT);
+//        
+//        else if (ev[i].type == EV_KEY && ev[i].code == 330 && ev[i].value == 0)
+//			printf("Event type is %s%s%s & Event code is %sTOUCH(330)%s & Event value is %s0%s = Touch Finished\n", KYEL,events[ev[i].type],KWHT,KYEL,KWHT,KYEL,KWHT);
+//        
+//        else if (ev[i].type == EV_ABS && ev[i].code == 0 && ev[i].value > 0){
+//            printf("Event type is %s%s%s & Event code is %sX(0)%s & Event value is %s%d%s\n", KYEL,events[ev[i].type],KWHT,KYEL,KWHT,KYEL,ev[i].value,KWHT);
+//			*rawX = ev[i].value;
+//		}
+//        else if (ev[i].type == EV_ABS  && ev[i].code == 1 && ev[i].value > 0){
+//            printf("Event type is %s%s%s & Event code is %sY(1)%s & Event value is %s%d%s\n", KYEL,events[ev[i].type],KWHT,KYEL,KWHT,KYEL,ev[i].value,KWHT);
+//			*rawY = ev[i].value;
+//		}
+//        else if (ev[i].type == EV_ABS  && ev[i].code == 24 && ev[i].value > 0){
+//            printf("Event type is %s%s%s & Event code is %sPressure(24)%s & Event value is %s%d%s\n", KYEL,events[ev[i].type],KWHT,KYEL,KWHT,KYEL,ev[i].value,KWHT);
+//			*rawPressure = ev[i].value;
+//		}
+//        
+//	}
+//}
+#endif
+
 void ofxPiTFT::setupTouchEvents(ofBaseApp *_app){
     app = _app;
 
     #ifdef TARGET_RASPBERRY_PI
     //  Touch
     //
-//    if (openTouchScreen() == 1)
-//		perror("error opening touch screen");
-//    
+//    if ((fd = open("/dev/input/event0", O_RDONLY)) < 0) {
+//        return 1;
+//    }
+//
 //    int screenXmax, screenXmin;
 //	int screenYmax, screenYmin;
-//    
+//
 //    getTouchScreenDetails(&screenXmin,&screenXmax,&screenYmin,&screenYmax);
 //    scaleXvalue = ((float)screenXmax-screenXmin) / 320.0;
 //    scaleYvalue = ((float)screenYmax-screenYmin) / 240.0;
